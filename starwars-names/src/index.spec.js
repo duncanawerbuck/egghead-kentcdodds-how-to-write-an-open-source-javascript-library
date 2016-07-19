f9484219d3a95f59195138f0fb4e099ebcba176d
githubuser919 <d_awerbuck@hotmail.com>
2016-07-19 19:07:27 +0000

    Testing with Mocha and Chai

---
+++ starwars-names/src/index.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect,
+    starWars = require('./index.js');
+
+describe('the simplest possible test', () => {
+    it('should work!', () => {
+        expect(true).to.be.true;
+    })
+});
