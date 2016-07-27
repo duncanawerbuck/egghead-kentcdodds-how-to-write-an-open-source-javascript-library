e4b5a1c7a16e777bef5fdefc4aecc6b3a169dca6
githubuser919 <d_awerbuck@hotmail.com>
2016-07-27 17:31:16 +0000

    First 'real' test

--- starwars-names/src/index.spec.js
+++ starwars-names/src/index.spec.js
@@ -1,8 +1,12 @@
 const expect = require('chai').expect,
     starWars = require('./index.js');
 
-describe('the simplest possible test', () => {
-    it('should work!', () => {
-        expect(true).to.be.true;
-    })
+describe('starwars-names', () => {
+    it('all returns array of strings', () => {
+        expect(starWars.all).to.satisfy(isArrayOfStrings);
+        
+        function isArrayOfStrings(array) {
+            return array.every((item) =>  typeof item === 'string' );
+        }
+    });
 });
